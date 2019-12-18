c8064427856155ed1c2506bdc34c3ed031db4383
Jon Patterson <JonPatt92@gmail.com>
2019-12-18 16:03:42 -0700

    Delete unecessary commented out code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@
 
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  ## Contained in config/environment.rb
-  # configure do
-  #   set :public_folder, 'public'
-  #   set :views, 'app/views'
-  # end
 
 end

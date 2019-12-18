89e3a24f16f324d9a4a7de8f4339b33c11d3a2bf
Jon Patterson <JonPatt92@gmail.com>
2019-12-17 19:13:00 -0700

    Create Application Controller. Configuration moved to environment.rb unsure if environment needs to be required. Routes go here

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+# require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  ## Contained in config/environment.rb
+  # configure do
+  #   set :public_folder, 'public'
+  #   set :views, 'app/views'
+  # end
+
+end

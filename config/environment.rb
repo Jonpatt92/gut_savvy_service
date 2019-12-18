eb1995cd09781073da2f2946ff2fc70ca42f32ff
Jon Patterson <JonPatt92@gmail.com>
2019-12-17 19:09:28 -0700

    Add environment configuration based off turing sinatra_project with require_all modification

---
+++ config/environment.rb
@@ -0,0 +1,31 @@
+require 'bundler'
+
+ENV['SINATRA_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "development"
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+# get the path of the root of the app
+# APP_ROOT = File.expand_path("..", __dir__)
+
+# require the controller(s)
+# Dir.glob(File.join(APP_ROOT, 'app', 'controllers', '*.rb')).each { |file| require file }
+
+# require the model(s)
+# Dir.glob(File.join(APP_ROOT, 'app', 'models', '*.rb')).each { |file| require file }
+
+# require database configurations
+# require File.join(APP_ROOT, 'config', 'database')
+
+# Requires all files in app directory (require_all gem)
+require_all 'app'
+
+# Requires database configurations (require_all gem)
+require_rel 'database'
+
+# configure ApplicationController settings
+class ApplicationController < Sinatra::Base
+  set :method_override, true
+  set :views, 'app/views'
+  set :public_folder, 'public'
+  # set :root, APP_ROOT
+end

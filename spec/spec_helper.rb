dd46ef65c5938503c804f58f8f0345be330d2536
John Travers <johnk.travers@gmail.com>
2020-01-08 16:47:54 -0700

    Remove vcr cassettes and vcr configuration for rspec
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require 'simplecov'
 require 'rack/test'
 require 'rspec'
 require 'capybara/dsl'
-require 'vcr'
 
 SimpleCov.start
 
@@ -18,9 +17,3 @@ end
 
 # For RSpec 2.x and 3.x
 RSpec.configure { |c| c.include RSpecMixin }
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  config.hook_into :webmock
-  config.filter_sensitive_data("<ENV['FDC_API_KEY']>") { ENV['FDC_API_KEY'] }
-end

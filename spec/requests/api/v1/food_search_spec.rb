c9dd01b896414d0eeda8ace60559bbe8a9649dfa
John Travers <johnk.travers@gmail.com>
2020-01-08 16:11:32 -0700

    Add test for food_search api endpoint passing in a valid upc code
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

---
+++ spec/requests/api/v1/food_search_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe 'Food Search API Endpoint', type: :request do
+  it 'shows the food name, brand, and list of ingredients' do
+    VCR.use_cassette('greek_yogurt_info') do
+      get '/api/v1/food_search?upc=818290013613'
+
+      expect(last_response).to be_successful
+
+      food_json = JSON.parse(last_response.body)
+
+      expect(food_json['data'].length).to eq(4)
+      expect(food_json['data']['name']).to eq('GREEK YOGURT')
+      expect(food_json['data']['brand']).to eq('Chobani, Inc.')
+      expect(food_json['data']['upc']).to eq('818290013613')
+      expect(food_json['data']['ingredients'].length).to eq(10)
+      expect(food_json['data']['ingredients'].last).to eq('GUAR GUM')
+    end
+  end
+
+  # it 'shows an error message if the upc is invalid' do
+  #   get '/api/v1/food_search?upc=4253'
+  #
+  #   expect(response).to be_successful
+  #
+  #   food_json = JSON.parse(last_response.body)
+  #
+  #   expect(food_json['errors'].length).to eq(1)
+  # end
+end

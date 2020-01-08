899907e683301f01a4d785054c9cd45e4f82db3c
John Travers <johnk.travers@gmail.com>
2020-01-08 16:48:54 -0700

    Modify tests to use webmock instead of vcr
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- spec/requests/api/v1/food_search_spec.rb
+++ spec/requests/api/v1/food_search_spec.rb
@@ -2,20 +2,24 @@ require 'spec_helper'
 
 RSpec.describe 'Food Search API Endpoint', type: :request do
   it 'shows the food name, brand, and list of ingredients' do
-    VCR.use_cassette('greek_yogurt_info') do
-      get '/api/v1/food_search?upc=818290013613'
+    json_response = File.read('spec/fixtures/greek_yogurt_info.json')
 
-      expect(last_response).to be_successful
+    stub_request(:post, "https://api.nal.usda.gov/fdc/v1/search?api_key=#{ENV['FDC_API_KEY']}")
+      .with(body: '{"generalSearchInput":"818290013613"}', headers: { 'Content-Type'=>'application/json' })
+      .to_return(status: 200, body: json_response)
 
-      food_json = JSON.parse(last_response.body)
+    get '/api/v1/food_search?upc=818290013613'
 
-      expect(food_json['data'].length).to eq(4)
-      expect(food_json['data']['name']).to eq('GREEK YOGURT')
-      expect(food_json['data']['brand']).to eq('Chobani, Inc.')
-      expect(food_json['data']['upc']).to eq('818290013613')
-      expect(food_json['data']['ingredients'].length).to eq(10)
-      expect(food_json['data']['ingredients'].last).to eq('GUAR GUM')
-    end
+    expect(last_response).to be_successful
+
+    food_json = JSON.parse(last_response.body)
+
+    expect(food_json['data'].length).to eq(4)
+    expect(food_json['data']['name']).to eq('GREEK YOGURT')
+    expect(food_json['data']['brand']).to eq('Chobani, Inc.')
+    expect(food_json['data']['upc']).to eq('818290013613')
+    expect(food_json['data']['ingredients'].length).to eq(10)
+    expect(food_json['data']['ingredients'].last).to eq('GUAR GUM')
   end
 
   # it 'shows an error message if the upc is invalid' do
--- spec/services/fdc_service_spec.rb
+++ spec/services/fdc_service_spec.rb
@@ -2,21 +2,25 @@ require 'spec_helper'
 
 RSpec.describe FDCService, type: :model do
   it 'food info' do
-    VCR.use_cassette('quaker_oats_bar_info') do
-      service = FDCService.new
-      expected_response = {
-        fdcId: 538677,
-        description: "BARS",
-        dataType: "Branded",
-        gtinUpc: "030000400036",
-        publishedDate: "2019-04-01",
-        brandOwner: "The Quaker Oats Company",
-        ingredients: "WHOLE GRAIN ROLLED OATS, SOY CRISP RICE (SOY PROTEIN ISOLATE, RICE STARCH), INVERT SUGAR, BROWN SUGAR, OLIGOFRUCTOSE, RAISINS, PECANS, SUGAR, WATER, POLYDEXTROSE, VEGETABLE SHORTENING (CANOLA OIL, PALM OIL, PALM KERNEL OIL), GLYCERIN, WHOLE OAT FLOUR. CONTAINS 2% OR LESS OF OAT BRAN CONCENTRATE, DRIED WHOLE EGGS, MODIFIED FOOD STARCH, DEXTROSE, CORN SYRUP, NATURAL FLAVOR, FRUCTOSE, MALTED BARLEY EXTRACT, CALCIUM CARBONATE, CINNAMON, SODIUM BICARBONATE, SALT, CORN FLOUR, SODIUM ALGINATE, MALIC ACID, SUNFLOWER OIL, ENZYME MODIFIED SOY PROTEIN, ALMONDS, ALPHA TOCOPHEROL ACETATE (VITAMIN E), NIACINAMIDE (VITAMIN B3), TOCOPHEROLS, SODIUM HEXAMETAPHOSPHATE, VITAMIN A PALMITATE, THIAMIN MONONITRATE (VITAMIN B1), RIBOFLAVIN (VITAMIN B2), PYRIDOXINE HYDROCHLORIDE (VITAMIN B6), CYANOCOBALAMIN (VITAMIN B12). ",
-        allHighlightFields: "<b>GTIN/UPC</b>: <em>030000400036</em>",
-        score: -747.62915
-      }
+    json_response = File.read('spec/fixtures/quaker_oats_bar_info.json')
 
-      expect(service.food_info('030000400036')).to eq(expected_response)
-    end
+    stub_request(:post, "https://api.nal.usda.gov/fdc/v1/search?api_key=#{ENV['FDC_API_KEY']}")
+      .with(body: '{"generalSearchInput":"030000400036"}', headers: { 'Content-Type'=>'application/json' })
+      .to_return(status: 200, body: json_response)
+
+    service = FDCService.new
+    expected_response = {
+      fdcId: 538677,
+      description: "BARS",
+      dataType: "Branded",
+      gtinUpc: "030000400036",
+      publishedDate: "2019-04-01",
+      brandOwner: "The Quaker Oats Company",
+      ingredients: "WHOLE GRAIN ROLLED OATS, SOY CRISP RICE (SOY PROTEIN ISOLATE, RICE STARCH), INVERT SUGAR, BROWN SUGAR, OLIGOFRUCTOSE, RAISINS, PECANS, SUGAR, WATER, POLYDEXTROSE, VEGETABLE SHORTENING (CANOLA OIL, PALM OIL, PALM KERNEL OIL), GLYCERIN, WHOLE OAT FLOUR. CONTAINS 2% OR LESS OF OAT BRAN CONCENTRATE, DRIED WHOLE EGGS, MODIFIED FOOD STARCH, DEXTROSE, CORN SYRUP, NATURAL FLAVOR, FRUCTOSE, MALTED BARLEY EXTRACT, CALCIUM CARBONATE, CINNAMON, SODIUM BICARBONATE, SALT, CORN FLOUR, SODIUM ALGINATE, MALIC ACID, SUNFLOWER OIL, ENZYME MODIFIED SOY PROTEIN, ALMONDS, ALPHA TOCOPHEROL ACETATE (VITAMIN E), NIACINAMIDE (VITAMIN B3), TOCOPHEROLS, SODIUM HEXAMETAPHOSPHATE, VITAMIN A PALMITATE, THIAMIN MONONITRATE (VITAMIN B1), RIBOFLAVIN (VITAMIN B2), PYRIDOXINE HYDROCHLORIDE (VITAMIN B6), CYANOCOBALAMIN (VITAMIN B12). ",
+      allHighlightFields: "<b>GTIN/UPC</b>: <em>030000400036</em>",
+      score: -747.62915
+    }
+
+    expect(service.food_info('030000400036')).to eq(expected_response)
   end
 end

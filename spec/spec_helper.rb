44c1c4fa1ec7655f5f469d1d4a93a581c4f26542
John Travers <johnk.travers@gmail.com>
2020-01-08 16:09:50 -0700

    Configure rspec using simplecov, rspecmixin and vcr
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,26 @@
-require_relative './config/environment'
-# require 'rspec'
+require './config/environment'
+require 'simplecov'
+require 'rack/test'
+require 'rspec'
 require 'capybara/dsl'
-require 'bundler'
+require 'vcr'
 
-ENV["RACK_ENV"] ||= "test"
-Bundler.require(:default, ENV["RACK_ENV"])
+SimpleCov.start
 
-Capybara.app = ApplicationController
+ENV['RACK_ENV'] = 'test'
 
-RSpec.configure do |c|
-  c.include Capybara::DSL
+require File.expand_path '../../gut_savvy_app.rb', __FILE__
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() Sinatra::Application end
+end
+
+# For RSpec 2.x and 3.x
+RSpec.configure { |c| c.include RSpecMixin }
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data("<ENV['FDC_API_KEY']>") { ENV['FDC_API_KEY'] }
 end

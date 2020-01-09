120d19dfac735fefc945c3aa5e4875225d29eb9e
John Travers <johnk.travers@gmail.com>
2020-01-08 20:13:42 -0700

    Modify format_json method to handle errors if upc code is invalid

--- app/serializers/food_info_serializer.rb
+++ app/serializers/food_info_serializer.rb
@@ -5,7 +5,7 @@ class FoodInfoSerializer
     @name = food_info[:description]
     @brand = food_info[:brandOwner]
     @upc = food_info[:gtinUpc]
-    @ingredients = format_ingredients(food_info[:ingredients])
+    @ingredients = format_ingredients(food_info[:ingredients]) if food_info[:ingredients]
   end
 
   def format_ingredients(ingredient_list)
@@ -19,6 +19,16 @@ class FoodInfoSerializer
   end
 
   def format_json
+    if ingredients
+      data_hash
+    else
+      error_hash
+    end
+  end
+
+  private
+
+  def data_hash
     {
       data: {
         name: name,
@@ -26,6 +36,15 @@ class FoodInfoSerializer
         upc: upc,
         ingredients: ingredients
       }
+    }.to_json
+  end
+
+  def error_hash
+    {
+      errors: [{
+        status: '404 Not Found',
+        title: 'Invalid UPC Code. Please try again.'
+      }]
     }.to_json
   end
 end

73bbf9807c69e91a070ec7769d5e22b2e83f63cd
Jon Patterson <JonPatt92@gmail.com>
2020-01-21 15:42:01 -0700

    Change how ingredients are processed. Add helper methods to consolidate appearances of different milk types and to remove water/natural flavor ingredients

--- app/serializers/food_info_serializer.rb
+++ app/serializers/food_info_serializer.rb
@@ -5,7 +5,39 @@ class FoodInfoSerializer
     @name = food_info[:description]
     @brand = food_info[:brandOwner]
     @upc = food_info[:gtinUpc]
-    @ingredients = format_ingredients(food_info[:ingredients]) if food_info[:ingredients]
+    @ingredients = process_ingredients(food_info[:ingredients]) if food_info[:ingredients]
+  end
+
+  def process_ingredients(ingredient_list)
+    formatted_ingredients    = format_ingredients(ingredient_list)
+    consolidated_ingredients = consolidate_ingredients(formatted_ingredients)
+    validate_ingredients(consolidated_ingredients).uniq
+  end
+
+  def consolidate_ingredients(ingredients)
+    ingredients.map! do |ingredient|
+      if ingredient.include?("MILK")
+        ingredient.replace("MILK") unless not_dairy_milk?(ingredient)
+      else
+        ingredient
+      end
+    end
+  end
+
+  def not_dairy_milk?(ingredient)
+    ingredient.include?("ALMOND")  ||
+    ingredient.include?("SOY")     ||
+    ingredient.include?("CASHEW")  ||
+    ingredient.include?("COCONUT") ||
+    ingredient.include?("FLAX")    ||
+    ingredient.include?("HEMP")    ||
+    ingredient.include?("RICE")    ||
+    ingredient.include?("OAT")
+  end
+
+  def validate_ingredients(ingredients)
+    ingredients.delete_if { |ingredient| ingredient.include?("WATER") }
+    ingredients.delete_if { |ingredient| ingredient.include?("NATURAL FLAVOR") }
   end
 
   def format_ingredients(ingredient_list)
